5523e295264d8c64fc8b6c65e1e6ed0432121fcd
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2024-02-17 17:58:15 +0400

    Update migration.py - migration ready (without migration table)

--- migration.py
+++ migration.py
@@ -3,7 +3,6 @@
 import pg8000
 
 def migrate_database():
-    # Connect to the PostgreSQL database
     conn = pg8000.connect(
         database="postgres",
         user="postgres",
@@ -14,12 +13,9 @@ def migrate_database():
     cur = conn.cursor()
 
     try:
-        # Rename ST_ID to STUDENT_ID
         if table_exists(cur, "students"):
-            # Rename ST_ID to STUDENT_ID
             cur.execute("ALTER TABLE STUDENTS RENAME COLUMN ST_ID TO STUDENT_ID")
 
-            # Change length of ST_NAME and ST_LAST columns
             cur.execute("ALTER TABLE STUDENTS ALTER COLUMN ST_NAME TYPE VARCHAR(30)")
             cur.execute("ALTER TABLE STUDENTS ALTER COLUMN ST_LAST TYPE VARCHAR(30)")
 
@@ -27,11 +23,9 @@ def migrate_database():
             print('no')
 
         if table_exists(cur, "interests"):
-            # Change name and type of INTEREST column
             cur.execute("ALTER TABLE INTERESTS RENAME COLUMN INTEREST TO INTERESTS")
             cur.execute("ALTER TABLE INTERESTS ALTER COLUMN INTERESTS TYPE TEXT[] USING array[INTERESTS]")
 
-            # Aggregate interests for each STUDENT_ID
             cur.execute("""
                 SELECT
                     STUDENT_ID,
@@ -52,9 +46,7 @@ def migrate_database():
 
             print(results)
 
-            # Update INTERESTS table with aggregated interests
             for student_id, interests in results:
-                # Flatten the nested arrays and convert to a string representation
                 flattened_interests = '{{' + ','.join([interest[0] for interest in interests]) + '}}'
                 cur.execute("UPDATE INTERESTS SET INTERESTS = %s WHERE STUDENT_ID = %s",
                             (flattened_interests, student_id))
@@ -72,7 +64,6 @@ def migrate_database():
 
 
 
-        # Commit the changes
         conn.commit()
         print("Migration completed successfully.")
 
@@ -81,19 +72,16 @@ def migrate_database():
         print("Error during migration:", e)
 
     finally:
-        # Close the database connection
         cur.close()
         conn.close()
 
 def table_exists(cursor, table_name):
-    """Check if the specified table exists in the database."""
     cursor.execute(
         "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = %s)",
         (table_name,)
     )
     return cursor.fetchone()[0]
 
-# Call the migrate_database function to execute the migration
 if __name__ == "__main__":
     migrate_database()
 
